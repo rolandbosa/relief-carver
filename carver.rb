47ebc51ec5b7101dcbcd94c4098b1924f417e55c
Roland Bosa <roland.bosa@gmail.com>
2013-05-09 00:52:46 -0700

    Added reverser and flip controls, new naming mechanism
    Also writing separate files for each slice.
    
    Signed-off-by: Roland Bosa <roland.bosa@gmail.com>

--- carver.rb
+++ carver.rb
@@ -4,14 +4,17 @@ require 'chunky_png'
 require 'ruby-progressbar'
 require 'pp'
 
+# Which relief are we carving?
+$Relief = 'sf'
+
 # Big picture / high resolution
-$ImageFilename = 'sf.png'
+# $ImageFilename = "#{$Relief}.png"
 
 # Small picture / low resolution
-# $ImageFilename = 'mini.png'
+$ImageFilename = "#{$Relief}-mini.png"
 
-# the file that will be filled with GCodes
-$GCodeFilename = 'data.txt'
+# Output filename prefix
+$GCodeFilenamePrefix = "#{$Relief}-gcode"
 
 # All measurements in inches, unless specified otherwise.
 
@@ -36,19 +39,38 @@ $CarveRadius = 1.00 /  8.00
 # to pass over a spot to reach maximum penetration depth...
 $CarveDepth  = 1.00 /  8.00
 
+# Height at which to taxi around
+$TaxiZ       = 2.00
+
+# How fast the tool moves during carving and while at
+# taxi-height. (G-code: F)
 $CarveSpeed  = 10.0
 $TaxiSpeed   = 30.0
 
+# Direction of cutting of the X- and Y-axis - if the tool does not
+# 'bite' into the material, you need to reverse one of these
+# directions.
+$ReverseX    = false
+$ReverseY    = true
+
+# Flipping the image, if it comes out mirrored, or upside down
+$FlipX       = true
+$FlipY       = false
+
+# Tiny amount to leave on surface when doing slice N, which will be
+# shaved off during slice N+1. Makes any visible marks for slices
+# disappear.
+$SliceShave  = 0.05
+
 #-------
 
-# Height at which to taxi around
-$TaxiZ       = 2.00
 $ReliefRange = $ReliefMaxZ - $ReliefMinZ
 
 # some range checks...
 raise "$ReliefMinZ must be smaller than $ReliefMaxZ!" if $ReliefMinZ >= $ReliefMaxZ
 raise "$BlockX should be a tad bigger (for numerical stability)!" if $BlockX < 0.01
 raise "$BlockY should be a tad bigger (for numerical stability)!" if $BlockY < 0.01
+raise "$SliceShave should be 0.0 or positive!" if $SliceShave < 0.0
 STDERR.puts "Warning - highest relief is higher than block!" if $BlockZ < $ReliefMaxZ
 
 #-------
@@ -127,6 +149,8 @@ def goto(file, x, y, z, s)
 end
 
 def maxHeight(x, y)
+  x = $BlockX - x if $FlipX
+  y = $BlockY - y if $FlipY
   cx = ($ImageOffsetX + x * $aspect).to_i
   cy = ($ImageOffsetY + y * $aspect).to_i
   radius = ($CarveRadius * $aspect).ceil
@@ -160,37 +184,44 @@ STDERR.puts "One pixel of the source image corresponds to #{1.0 / $aspect} inche
 STDERR.puts "Advancing by 1/4 of the tool radius amounts to #{$CarveRadius / 4.0} inches."
 carveStep = [1.0 / $aspect, $CarveRadius / 4.0].max
 STDERR.puts "Using a carver step size of: #{carveStep} inches."
-totalSteps = (($BlockX / carveStep).ceil * ($BlockY / carveStep).ceil).to_i
+stepsX = ($BlockX / carveStep).ceil
+stepsY = ($BlockY / carveStep).ceil
+totalSteps = stepsX * stepsY
+STDERR.puts "Using a shaving size of #{$SliceShave} inches." if 0.0 < $SliceShave
 
 # lets do it!
-File.open($GCodeFilename, "w") do |file|
-  goto file, -$OffsetX, -$OffsetY, $TaxiZ, $TaxiSpeed
-  while 0 < sliceCount do
-    progress = ProgressBar.create(:format => '%e |%b>>%i| %P%% %c/%C %t',
-                                  :title => "Slice #{sliceCount}",
-                                  :total => totalSteps)
+while 0 < sliceCount do
+  progress = ProgressBar.create(:format => '%e |%b>>%i| %P%% %c/%C %t',
+                                :title => "Slice #{sliceCount}",
+                                :total => totalSteps)
+  File.open("#{$GCodeFilenamePrefix}-#{sliceCount}.txt", "w") do |file|
+    goto file, -$OffsetX, -$OffsetY, $TaxiZ, $TaxiSpeed
     sliceCount -= 1
     sliceFloor = $ReliefMinZ + sliceCount * $CarveDepth
-    carveX = 0.0
-    while carveX < $BlockX do
-      carveY = 0.0
+    shaving = sliceCount * $SliceShave
+    x = 0
+    while x < stepsX do
+      carveX = ($ReverseX ? ((stepsX - 1) - x) : x) * carveStep
+      y = 0
+      carveY = ($ReverseY ? ((stepsY - 1) - y) : y) * carveStep
       goto file, carveX, carveY, $TaxiZ, $TaxiSpeed
-      while carveY < $BlockY do
+      while y < stepsY do
         redValue = maxHeight(carveX, carveY)
         reliefZ = $ReliefMinZ + ($ReliefRange * (redValue - $minRed)) / $rangeRed
         if reliefZ < sliceFloor then reliefZ = sliceFloor end
+        reliefZ += shaving
         goto file, carveX, carveY, reliefZ, $CarveSpeed
         progress.increment if progress.progress < (totalSteps - 1)
-        carveY += carveStep
+        y += 1
+        carveY = ($ReverseY ? ((stepsY - 1) - y) : y) * carveStep
       end
       goto file, carveX, carveY, $TaxiZ, $TaxiSpeed
-      carveX += carveStep
+      x += 1
     end
     progress.finish
+    # go back to rest position
+    STDERR.puts "Moving back to origin..."
+    goto file, -$OffsetX, -$OffsetY, $TaxiZ, $TaxiSpeed
+    goto file, -$OffsetX, -$OffsetY, 0.0, $TaxiSpeed
   end
-
-  # go back to rest position
-  STDERR.puts "Moving back to origin..."
-  goto file, -$OffsetX, -$OffsetY, $TaxiZ, $TaxiSpeed
-  goto file, -$OffsetX, -$OffsetY, 0.0, $TaxiSpeed
 end
